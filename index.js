3dce8d590541d78e74656b08a2a116faeb45b9e9
Michael Phan <michaelalexanderphan@gmail.com>
2023-09-26 15:41:28 -0700

    Ingredient List finished

--- index.js
+++ index.js
@@ -1,47 +1,75 @@
-//Global variables
-const URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita'
-const margaritaDropDown = document.querySelector('#margarita-dropdown')
-const commentForm = document.querySelector('#comment-form')
-const commentValue = document.querySelector('#comment-input')
-//Landon
+// Global variables
+const URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
+const margaritaDropDown = document.querySelector('#margarita-dropdown');
+const commentForm = document.querySelector('#comment-form');
+const commentValue = document.querySelector('#comment-input');
+const ingredientsList = document.querySelector('#ingredients-list');
+
+// Landon
 const start = () => {
-    fetch(URL)
-        .then(response => response.json())
-        .then(data => data.drinks.forEach(margarita => dropMenue(margarita)))
-        .catch(error => alert(error))
-}
-const dropMenue = (margarita) => {
-  
-    const option = document.createElement('option')
-    option.textContent = margarita.strDrink
-    margaritaDropDown.append(option)
-}
-start()
-//Michel
+  fetch(URL)
+    .then(response => response.json())
+    .then(data => data.drinks.forEach(margarita => dropMenu(margarita)))
+    .catch(error => alert(error));
+};
 
+const dropMenu = (margarita) => {
+  const option = document.createElement('option');
+  option.textContent = margarita.strDrink;
+  margaritaDropDown.append(option);
+};
 
+function handleCocktailSelection() {
+  const selectedCocktail = margaritaDropDown.value;
 
-
-//Kimberly
-
-
-document.addEventListener("DOMContentLoaded", () => {
-    const commentForm = document.querySelector('#comment-form')
-    commentForm.addEventListener('submit', (event) => {event.preventDefault()
-        const commentValue = document.querySelector('#comment-input')
-        console.log(commentValue.value)
-        buildComment(commentValue.value)
-        console.log(event.target.id)
-        commentForm.reset()
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${selectedCocktail}`)
+    .then(response => response.json())
+    .then(data => {
+      const selectedCocktailData = data.drinks[0];
+      displayIngredients(selectedCocktailData);
     })
-})
+    .catch(error => alert(error));
+}
 
+margaritaDropDown.addEventListener('change', handleCocktailSelection);
 
-function buildComment (comment) {
-    let li = document.createElement('li');
-    li.textContent = (`${comment}`)
-    document.querySelector('#comment-list').append(li)
+// Function to display ingredients
+const displayIngredients = (cocktail) => {
+  ingredientsList.innerHTML = '';
+
+  for (let i = 1; i <= 15; i++) {
+    const ingredient = cocktail[`strIngredient${i}`];
+    const measure = cocktail[`strMeasure${i}`];
+
+    if (ingredient) {
+      const li = document.createElement('li');
+      li.textContent = `${measure ? measure + ' ' : ''}${ingredient}`;
+      ingredientsList.appendChild(li);
+    } else {
+      break;
+    }
   }
+};
+
+// Kimberly
+document.addEventListener('DOMContentLoaded', () => {
+  const commentForm = document.querySelector('#comment-form');
+  commentForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const commentValue = document.querySelector('#comment-input');
+    buildComment(commentValue.value);
+    commentForm.reset();
+  });
+
+  function buildComment(comment) {
+    let li = document.createElement('li');
+    li.textContent = `${comment}`;
+    document.querySelector('#comment-list').append(li);
+  }
+});
+
+start();
+
 
 //document.addEventListener("DOMContentLoaded", () { })  //first event
 
