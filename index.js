5146af9d15767b15dcf85d82732ab33debe1635e
Landon <landoncramer1@gmail.com>
2023-09-27 09:31:07 -0700

    add css

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-
 //Global variables
 const URL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 const listDrinks = document.querySelector("#drink-list");
 const commentForm = document.querySelector("#comment-form");
 const commentValue = document.querySelector("#comment-input");
 const drinkSearch = document.querySelector("#drink-search");
-const searchResultsContainer = document.querySelector('#search-results');
-const likes = document.querySelector('#likes')
-const addLike = document.querySelector('#add-like')
-const resetLike = document.querySelector('#reset-btn')
-const addCocktailMessage = document.querySelector('#drink-search input[type=text]')
-const list = document.querySelector('li')
+const searchResultsContainer = document.querySelector("#search-results");
+const likes = document.querySelector("#likes");
+const addLike = document.querySelector("#add-like");
+const resetLike = document.querySelector("#reset-btn");
+const addCocktailMessage = document.querySelector(
+  "#drink-search input[type=text]"
+);
+const list = document.querySelector("li");
 //Landon
 drinkSearch.addEventListener("submit", (e) => {
   e.preventDefault();
   let request = e.target.search.value;
-if(request.length > 1) {
-  start(request);
-  e.target.reset()
-} else {
-addCocktailMessage.placeholder = "Add Cocktail.."
-}
+  if (request.length > 1) {
+    start(request);
+    e.target.reset();
+  } else {
+    addCocktailMessage.placeholder = "Add Cocktail..";
+  }
 });
 
 const start = (request) => {
@@ -42,73 +43,68 @@ const drinkList = (drink) => {
   const li = document.createElement("li");
   li.textContent = drink.strDrink;
   listDrinks.appendChild(li);
-  li.addEventListener('mousemove', mouseOver)
-  li.addEventListener('mouseout', mouseOut)
-  li.addEventListener('click', () => {
+  li.addEventListener("mousemove", mouseOver);
+  li.addEventListener("mouseout", mouseOut);
+  li.addEventListener("click", () => {
     displayIngredients(drink);
+    likes.textContent = 0;
   });
 };
 
 const mouseOver = (e) => {
-  e.target.style.color = 'blue'
-}
+  e.target.style.color = "blue";
+};
 const mouseOut = (e) => {
-  e.target.style.color = ""
-}
+  e.target.style.color = "";
+};
 
 const likeDrink = () => {
-likes.textContent = parseInt(likes.textContent) + 1
-}
+  likes.textContent = parseInt(likes.textContent) + 1;
+};
 
 const resetDrink = () => {
-  likes.textContent = 0
-  }
+  likes.textContent = 0;
+};
 
-  // list.addEventListener('mousemove', mouse)
-  addLike.addEventListener('click', likeDrink)
-  resetLike.addEventListener('click', resetDrink)
+addLike.addEventListener("click", likeDrink);
+resetLike.addEventListener("click", resetDrink);
 
 //Michel
 
-
 const displayIngredients = (cocktail) => {
-  const ingredientsList = document.querySelector('#ingredients-list');
+  const ingredientsList = document.querySelector("#ingredients-list");
   ingredientsList.innerHTML = "";
 
   for (let i = 1; i <= 15; i++) {
-      const ingredient = cocktail[`strIngredient${i}`];
-      const measure = cocktail[`strMeasure${i}`];
+    const ingredient = cocktail[`strIngredient${i}`];
+    const measure = cocktail[`strMeasure${i}`];
 
-      if (ingredient) {
-          const li = document.createElement("li");
-          li.textContent = `${measure ? measure + " " : ""}${ingredient}`;
-          ingredientsList.appendChild(li);
-      } else {
-          break;
-      }
+    if (ingredient) {
+      const li = document.createElement("li");
+      li.textContent = `${measure ? measure + " " : ""}${ingredient}`;
+      ingredientsList.appendChild(li);
+    } else {
+      break;
+    }
   }
 };
 
-
 // Kimberly
-document.addEventListener('DOMContentLoaded', () => {
-  const commentForm = document.querySelector('#comment-form');
-  commentForm.addEventListener('submit', (event) => {
+document.addEventListener("DOMContentLoaded", () => {
+  const commentForm = document.querySelector("#comment-form");
+  commentForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    const commentValue = document.querySelector('#comment-input');
+    const commentValue = document.querySelector("#comment-input");
     buildComment(commentValue.value);
     commentForm.reset();
   });
 
   function buildComment(comment) {
-    let li = document.createElement('li');
+    let li = document.createElement("li");
     li.textContent = `${comment}`;
-    document.querySelector('#comment-list').append(li);
+    document.querySelector("#comment-list").append(li);
   }
 });
-
-
-
 
 document.addEventListener("DOMContentLoaded", () => {
   const commentForm = document.querySelector("#comment-form");
@@ -128,7 +124,6 @@ function buildComment(comment) {
   document.querySelector("#comment-list").append(li);
 }
 
-
 //document.addEventListener("DOMContentLoaded", () { })  //first event
 
 //Project Assignments List
@@ -143,4 +138,3 @@ function buildComment(comment) {
 //fetch request here
 
 // fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
-
