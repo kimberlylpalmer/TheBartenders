7ad614196a25e1b5593b90b7814f1e314dfbc614
Kimberly L Palmer <kimberlylpalmer@gmail.com>
2023-09-27 10:19:24 -0700

    New logo

--- index.js
+++ index.js
@@ -129,10 +129,10 @@ function buildComment(comment) {
 //document.addEventListener("DOMContentLoaded", () { })  //first event
 
 //Project Assignments List
-//1. DOMContentLoaded - Page Loads and picture pops up - Doesn't count as an eventListener
-//2. MouseOver - Button will be in the heading that will open the drop down menu of
-//3. Click - Whatever is selected from drop down menu, after a "click" will display name, image, description
-//4. Keypress Event - create like button spacebar press event
+//1. COMPLETED WITH SCRIPT PLACEMENT - DOMContentLoaded - Page Loads and picture pops up - Doesn't count as an eventListener
+//2. COMPLETED - MouseOver - Button will be in the heading that will open the drop down menu of
+//3. COMPLETED - Click - Whatever is selected from drop down menu, after a "click" will display name, image, description
+//4. MouseOut  - Mouseover the drinks and changes colors/
 //5. COMPLETED - Submit - create comment form, add comment below the instructions
 //6. Language Menu
 //7. Add comments to API persistently
